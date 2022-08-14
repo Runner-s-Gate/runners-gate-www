0d7f61706144b233b836eb94c7fb008feb504e59
Alexander Hjelm <alexander.hjelm@solidify.dev>
2022-08-14 13:54:41 +0200

    Edit landing page. Update download links

--- src/App.js
+++ src/App.js
@@ -6,16 +6,53 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>
+          Download game client
+        </h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Choose your platform:
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://runnersgatesa.blob.core.windows.net/release/runners-gate-windows.zip"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Windows (64 bit .exe)
+        </a>
+
+        <a
+          className="App-link"
+          href="https://runnersgatesa.blob.core.windows.net/release/runners-gate-linux.zip"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GNU/Linux (Ubuntu, x86_64 executable)
+        </a>
+
+        <a
+          className="App-link"
+          href="https://runnersgatesa.blob.core.windows.net/release/runners-gate-mac.zip"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MacOS (.app bundle)
+        </a>
+
+        <p>
+          Contact: alexander.hjelm@solidify.dev
+        </p>
+
+        <a
+          className="App-link"
+          href="https://github.com/Runner-s-Gate"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
         </a>
       </header>
     </div>
